2e0541b3cfe7071bacc4f09faaa80ed2188241f7
Robin Dahan <robin.dahan@edhec.com>
2021-06-22 18:33:39 +0200

    navbar beta

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,7 +38,7 @@ puts "=== Adding Activity... ==="
 activity1 = Activity.create(
   name: "Padel tennis",
   address: "6 Rue Abel Gance 78390 Bois-d’Arcy",
-  type: "indoor",
+  indoor: true,
   category:"sport",
   price_per_head:"X",
   contact_email:25,
@@ -46,7 +46,7 @@ activity1 = Activity.create(
   activity_url:"http://le-padel-club.com/",
   longitude: 2.3646578788757324,
   latitude: 48.86328125,
-  user_id: user1.id)
+  )
 # boat1.photo.attach(io: URI.open('https://images.unsplash.com/photo-1586364312368-3d61a0ec826d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNhaWxib2F0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'), filename: 'X.jpg', content_type: 'image/jpg')
 # boat1.save
 # boat2 = Boat.create(title: "Best boat ever had",category:"🚤 motorboat",brand:"Beneteau",location:"Martigues", length:12,price_per_day:100,capacity:5, building_year:2012, user_id: user2.id)
