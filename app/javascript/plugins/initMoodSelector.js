4de9ba765901c79f3faf17b1d2ad5e3cc3d20304
taniahajjar <tania.hajjar@edhec.com>
2021-06-24 14:17:45 +0200

    mood cards fonctionnelles

--- app/javascript/plugins/initMoodSelector.js
+++ app/javascript/plugins/initMoodSelector.js
@@ -2,7 +2,9 @@ const initMoodSelector = () => {
   const divRandom = document.querySelector(".completely-random");
   const divCustom = document.querySelector(".choose-a-little");
   const divSentence = document.getElementById("mood-sentence");
-  console.log(divCustom);
+  const btnRandomize = document.querySelector(".button-randomize");
+  const btnSuivant = document.querySelector(".button-suivant");
+
   if(divSentence){
     const imageAdv = document.querySelector(".img-adventure");
     imageAdv.addEventListener("click", (event) => {
@@ -12,6 +14,8 @@ const initMoodSelector = () => {
       imageAdv.style.border = "3px solid #ffc300";
       imageAdv.style.borderRadius = '3px';
       imageShy.style.border = "none";
+      btnRandomize.style.display = "block";
+      btnSuivant.style.display = "none";
     });
 
     const imageShy = document.querySelector(".img-shy");
@@ -21,6 +25,8 @@ const initMoodSelector = () => {
       imageShy.style.border = "3px solid #ffc300";
       imageShy.style.borderRadius = '3px';
       imageAdv.style.border = "none";
+      btnRandomize.style.display = "none";
+      btnSuivant.style.display = "block";
     });
   }
 }
