ac6f5e537bb7c7f063c13f5d88a910862bc60846
Louis Hocquet <louis.hocquet@edhec.com>
2021-06-24 14:34:19 +0200

    Fixed seed cleaning order

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,16 +12,20 @@ require_relative 'scrapping/scrap_lechti'
 
 puts "=== Cleaning database... ==="
 
-Activity.destroy_all
-puts "=== Activity clean! ==="
-Slot.destroy_all
-puts "=== Slot clean! ==="
 Review.destroy_all
 puts "=== Review clean! ==="
 Notification.destroy_all
 puts "=== Notification clean! ==="
+
 Booking.destroy_all
 puts "=== Booking clean! ==="
+
+Slot.destroy_all
+puts "=== Slot clean! ==="
+
+Activity.destroy_all
+puts "=== Activity clean! ==="
+
 User.destroy_all
 puts "=== User clean! ==="
 
