d5f883a9ff774745b8b2b82a977b090701864878
taniahajjar <tania.hajjar@edhec.com>
2021-06-24 13:53:32 +0200

    border ok

--- app/javascript/plugins/initMoodSelector.js
+++ app/javascript/plugins/initMoodSelector.js
@@ -9,12 +9,18 @@ const initMoodSelector = () => {
       console.log(divRandom)
       divRandom.style.display = "block";
       divCustom.style.display = "none";
+      imageAdv.style.border = "3px solid #ffc300";
+      imageAdv.style.borderRadius = '3px';
+      imageShy.style.border = "none";
     });
 
     const imageShy = document.querySelector(".img-shy");
     imageShy.addEventListener("click", (event) => {
       divRandom.style.display = "none";
       divCustom.style.display = "block";
+      imageShy.style.border = "3px solid #ffc300";
+      imageShy.style.borderRadius = '3px';
+      imageAdv.style.border = "none";
     });
   }
 }
