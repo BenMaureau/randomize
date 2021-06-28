36c2bef4cc3b14e54fb79a74fa3bac3147154771
Louis Hocquet <louis.hocquet@edhec.com>
2021-06-28 11:09:41 +0200

    Refactored init_mapbox js code

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,20 +5,27 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
-  const fitMapToMarkers = (map, markers) => {
-    const bounds = new mapboxgl.LngLatBounds();
-    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-  };
+  // const fitMapToMarkers = (map, markers) => {
+  //   const bounds = new mapboxgl.LngLatBounds();
+  //   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  //   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  // };
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    let lon = 3.066667;
+    let lat = 50.633333;
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10',
-      center: [3.066667,50.633333],
+      center: [lon ,lat], // center on Lille
       zoom: 12
     });
+
+    
+    let marker = createPerimeterMarker();
+
+    setPerimeterMarker(map, marker, lon, lat);
     const current = document.querySelector('#geolocate');
     current.addEventListener('click', (event) => {
       console.log(event);
@@ -30,67 +37,47 @@ const initMapbox = () => {
           container: 'map',
           style: 'mapbox://styles/mapbox/streets-v10',
           center: [lon,lat],
-          zoom: 13
+          zoom: 12
         });
-        const element = document.createElement('div');
-        element.className = 'marker';
-        element.style.backgroundImage = `url('Ellipse.png')`;
-        element.style.backgroundSize = 'contain';
-        element.style.width = '50px';
-        element.style.height = '50px';
-        element.style.backgroundColor = "#ffc300";
-        element.style.opacity = "50%";
-        element.style.borderRadius = "50px";
-        element.style.border = "1px solid #003566"
-        new mapboxgl.Marker(element)
-          .setLngLat([lon, lat])
-          .addTo(map);
-        const markers = JSON.parse(mapElement.dataset.markers);
-        markers.forEach((marker) => {
-          new mapboxgl.Marker()
-          .setLngLat([marker.lng, marker.lat])
-          .addTo(map);
-        });
-        // fitMapToMarkers(map, markers);
-        map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-                                      mapboxgl: mapboxgl }));
-        // map.setZoom(8);
-        const rangeInput = document.getElementById("formControlRange");
+
+        setPerimeterMarker(map, marker, lon, lat);
       });
     });
-
-
-    const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-
-      // Create a HTML element for your custom marker
-      // const element = document.createElement('div');
-      // element.className = 'marker';
-      // element.style.backgroundImage = `url('${marker.image_url}')`;
-      // element.style.backgroundSize = 'contain';
-      // element.style.width = '50px';
-      // element.style.height = '50px';
-      // element.style.backgroundColor = "#ffc300";
-      // element.style.opacity = "20%";
-      // element.style.borderRadius = "50px";
-      // element.style.border = "1px solid #003566"
-
-      // Pass the element as an argument to the new marker
-      // new mapboxgl.Marker(element)
-      new mapboxgl.Marker()
-        .setLngLat([marker.lng, marker.lat])
-        .addTo(map);
-    });
-    // fitMapToMarkers(map, markers);
     map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
                                       mapboxgl: mapboxgl }));
     // map.setZoom(8);
-    const rangeInput = document.getElementById("formControlRange");
+    // const rangeInput = document.getElementById("formControlRange");
 
     // rangeInput.addEventListener("change", function() {
     //   map.setZoom(parseInt(rangeInput.value,10)/6);
     // }, false);
   }
 };
+
+const createPerimeterMarker = () => {
+  const element = document.createElement('div');
+  element.id = 'markerPerimeter';
+  element.style.backgroundColor = "#FFBF00";
+  element.style.border = "solid 2px #FF7145";
+  // element.style.backgroundSize = 'contain';
+  element.style.width = '50px';
+  element.style.height = '50px';
+  // element.style.backgroundColor = "#ffc300";
+  element.style.opacity = "50%";
+  element.style.borderRadius = "50%";
+  return element;
+};
+
+const setPerimeterMarker = (map, marker, lon, lat) => {
+  if(marker) {
+    marker.style.display = 'none';
+    new mapboxgl.Marker(marker)
+      .setLngLat([lon, lat])
+      .addTo(map);
+    marker.style.display = 'block';
+  }
+  // element.style.border = "1px solid #003566"
+};
+
 
 export { initMapbox };
