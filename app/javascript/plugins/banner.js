be247dceb99263c07047a3612e7f49b76b900760
Louis Hocquet <louis.hocquet@edhec.com>
2021-06-28 09:12:44 +0200

    canceled change on banner.js

--- app/javascript/plugins/banner.js
+++ app/javascript/plugins/banner.js
@@ -3,20 +3,16 @@ import Typed from 'typed.js';
 const bannerText = document.getElementById('banner-typed-text');
 
 const loadDynamicBannerText = () => {
-  document.addEventListener("DOMContentLoaded", function(event) {
-    // do typed.js stuff
-    if(bannerText) {
-      console.log('coucou');
-      const activities = bannerText.dataset.activities;
-      const dynamicWords = activities.split(',');
-      new Typed('#banner-typed-text', {
-        strings: dynamicWords,
-        typeSpeed: 50,
-        loop: true
-      });
-    }
-  });
-
+  if(bannerText) {
+    console.log('coucou');
+    const activities = bannerText.dataset.activities;
+    const dynamicWords = activities.split(',');
+    new Typed('#banner-typed-text', {
+      strings: dynamicWords,
+      typeSpeed: 50,
+      loop: true
+    });
+  }
 }
 
 export { loadDynamicBannerText };
