36d9fee45b0f7b7ef3438d8a2c2761869d3ef44e
taniahajjar <tania.hajjar@edhec.com>
2021-06-29 14:39:52 +0200

    review conditions, maintenant ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -30,22 +30,25 @@ class PagesController < ApplicationController
   end
 
   def custom_activity
-    cat = []
-    cat << "Sport" if params[:sport] == "true"
-    cat << "Manger" if params[:manger] == "true"
-    cat << "Sortir" if params[:sortir] == "true"
-    cat << "Nature" if params[:nature] == "true"
-    cat << "Bien être" if params[:bien_etre] == "true"
-    cat << "Se divertir" if params[:se_divertir] == "true"
-    @activities = []
-    cat.each do |c|
-      @activities_filtered = Activity.where(indoor: params[:indoor]).where("price_per_head <= ?", params[:max_price].to_i).where(category: c)
-      @activities_filtered.each do |activity|
-        @activities << activity
+    if params[:aventurier] == "true"
+      select
+    else
+      cat = []
+      cat << "Sport" if params[:sport] == "true"
+      cat << "Manger" if params[:manger] == "true"
+      cat << "Sortir" if params[:sortir] == "true"
+      cat << "Nature" if params[:nature] == "true"
+      cat << "Bien être" if params[:bien_etre] == "true"
+      cat << "Se divertir" if params[:se_divertir] == "true"
+      @activities = []
+      cat.each do |c|
+        @activities_filtered = Activity.where(indoor: params[:indoor]).where("price_per_head <= ?", params[:max_price].to_i).where(category: c)
+        @activities_filtered.each do |activity|
+          @activities << activity
+        end
       end
+      @activity = @activities.sample
+      redirect_to activity_path(@activity) if @activities.size > 0
     end
-    @activity = @activities.sample
-    redirect_to activity_path(@activity) if Dir.exist?('activity_path') == true
   end
 end
-
