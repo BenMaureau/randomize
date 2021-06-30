1a9497676d1c6741ed586f806a2f765ce1ac1673
Louis Hocquet <louis.hocquet@edhec.com>
2021-06-30 12:03:01 +0200

    At last a working carousel

---
+++ app/javascript/plugins/init_carousel.js
@@ -0,0 +1,36 @@
+const initCarousel = () => {
+  $(document).ready(function(){
+    $('.my-carousel').slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+  });
+};
+
+export { initCarousel } 
