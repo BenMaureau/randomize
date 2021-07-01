17d4a39d993d34013d3bf49bfffe6a378658cc44
Louis Hocquet <50288549+LouisHocquet@users.noreply.github.com>
2021-07-01 12:38:28 +0200

    Update init_reveal.js
    Removed audio

--- app/javascript/plugins/init_reveal.js
+++ app/javascript/plugins/init_reveal.js
@@ -2,8 +2,6 @@ const initReveal = () => {
   const reveal = document.querySelector('.my-btn-reveal')
   if (reveal) {
     reveal.addEventListener('click', (event) => {
-      const audio = new Audio('/assets/reveal.mp3');
-      audio.play();
       const card = document.querySelector('.my-activity-card')
       event.preventDefault();
       card.classList.remove("activity-secret");
